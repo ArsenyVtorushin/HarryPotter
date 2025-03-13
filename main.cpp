61955e87e6b85131737fe8561631ac8965ed98e1
Arseny Vtorushi <vtorushinarseny@yandex.ru>
2025-03-13 21:12:18 +0500

    заполнены контейнеры и произведены замеры времени выполнения программы

--- main.cpp
+++ main.cpp
@@ -7,13 +7,18 @@
 #include <vector>
 #include <string>
 #include <set>
+#include<thread>
 
 std::string Convert(std::string str);
 
 int main()
 {
+	::setlocale(LC_ALL, "ru");
+
 	std::vector<std::string> vector;
 	std::set<std::string> set;
+
+	auto start_point = std::chrono::system_clock::now();
 
 	std::ifstream in_file;
 	in_file.open("J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt");
@@ -24,14 +29,6 @@ int main()
 	}
 	else
 	{
-		std::chrono::time_point<std::chrono::steady_clock> start_vec;
-		std::chrono::time_point<std::chrono::steady_clock> end_vec;
-		std::chrono::time_point<std::chrono::steady_clock> start_set;
-		std::chrono::time_point<std::chrono::steady_clock> end_set;
-
-
-		start_vec = std::chrono::high_resolution_clock::now();
-
 		std::string str;
 		while (!in_file.eof())
 		{
@@ -48,44 +45,18 @@ int main()
 				if (data != "")
 				{
 					vector.push_back(Convert(data));
-				}			
-			}
-		}
-
-		end_vec = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<float> duration_vec = end_vec - start_vec;
-		std::cout << "Duration of vector: " << duration_vec.count() << " seconds\n";
-
-		in_file.close();
-
-
-		start_set = std::chrono::high_resolution_clock::now();
-
-		while (!in_file.eof())
-		{
-			str = "";
-			std::getline(in_file, str, '\n');
-			std::stringstream str_stream(str);
-			std::string data;
-
-			while (!str_stream.eof())
-			{
-				data = "";
-				std::getline(str_stream, data, ' ');
-
-				if (data != "")
-				{
 					set.insert(Convert(data));
 				}
 			}
 		}
-
-		end_set = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<float> duration_set = end_set - start_set;
-		std::cout << "Duration of set: " << duration_set.count() << " seconds\n";
 	}
 
 	in_file.close();
+
+	auto end_point = std::chrono::system_clock::now();
+	auto t_diff = std::chrono::duration_cast<std::chrono::milliseconds>(end_point - start_point);
+
+	std::cout << "Ќа заполнение контейнеров ушло: " << t_diff.count() << " миллисекунд\n";
 
 	return 0;
 }
